aa19c7122462f14843c2096f37c885dd7e93df10
Alig577 <ak9063052@gmail.com>
2025-05-18 22:33:11 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    const faders = document.querySelectorAll(".fade-in");
+    const appearOptions = { threshold: 0.1 };
+    const appearOnScroll = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) return;
+            entry.target.classList.add("show");
+            observer.unobserve(entry.target);
+        });
+    }, appearOptions);
+    faders.forEach(fader => appearOnScroll.observe(fader));
+});
